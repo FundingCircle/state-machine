9ef3ce365c89f21967594380331425bb7cab8aed
ahmedsamy <ahmed.samy.cs@gmail.com>
2015-07-21 15:55:07 +0200

    [Bundle] Added created_at to transition class

--- src/StateMachineBundle/Entity/Transition.php
+++ src/StateMachineBundle/Entity/Transition.php
@@ -9,4 +9,30 @@ class Transition extends BaseTransition
      * @var int
      */
     protected $id;
+
+    /**
+     * @var \DateTime
+     */
+    protected $createdAt;
+
+    public function __construct()
+    {
+        $this->createdAt = new \DateTime();
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * @param \DateTime $createdAt
+     */
+    public function setCreatedAt($createdAt)
+    {
+        $this->createdAt = $createdAt;
+    }
 }
