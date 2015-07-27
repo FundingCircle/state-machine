ed022c778921b3c3118fb113ab261ccff0e80fd4
ahmedsamy <ahmed.samy.cs@gmail.com>
2015-07-27 20:41:32 +0200

    [Bundle] added twig extension
    - Added statemachine twig extenstion
    
    - added  "renderGraph" filter to render object statemachine

---
+++ src/StateMachineBundle/Twig/StateMachineExtension.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace StateMachineBundle\Twig;
+
+use StateMachine\StateMachine\StatefulInterface;
+use StateMachine\Visualisation\Configuration;
+use StateMachine\Visualisation\Graphviz;
+
+class StateMachineExtension extends \Twig_Extension
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getFilters()
+    {
+        return [
+            new \Twig_SimpleFilter('renderGraph', [$this, 'renderGraphFilter'], ['is_safe' => ['html']]),
+        ];
+    }
+
+    public function renderGraphFilter(StatefulInterface $statefulObject)
+    {
+        $config = new Configuration(false, 'red');
+        $graphviz = new Graphviz($config);
+
+        return $graphviz->render($statefulObject->getStateMachine());
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return "statemachine";
+    }
+}
