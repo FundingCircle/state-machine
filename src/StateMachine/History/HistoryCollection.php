692fd46f7aba17661da9980a1a16b8f3c018c117
ahmedsamy <ahmed.samy.cs@gmail.com>
2016-01-15 10:06:19 +0100

    [Lib] History Collection is an ArrayCollection

--- src/StateMachine/History/HistoryCollection.php
+++ src/StateMachine/History/HistoryCollection.php
@@ -2,70 +2,9 @@
 
 namespace StateMachine\History;
 
-class HistoryCollection implements \Countable
+use Doctrine\Common\Collections\ArrayCollection;
+
+class HistoryCollection extends ArrayCollection
 {
-    /**
-     * An array containing the entries of this collection.
-     *
-     * @var array
-     */
-    private $elements;
 
-    /**
-     * Initializes a new ArrayCollection.
-     *
-     * @param array $elements
-     */
-    public function __construct(array $elements = array())
-    {
-        $this->elements = $elements;
-    }
-
-    /**
-     * (PHP 5 &gt;= 5.1.0)<br/>
-     * Count elements of an object.
-     *
-     * @link http://php.net/manual/en/countable.count.php
-     *
-     * @return int The custom count as an integer.
-     *             </p>
-     *             <p>
-     *             The return value is cast to an integer.
-     */
-    public function count()
-    {
-        return count($this->elements);
-    }
-
-    /**
-     * @return array
-     */
-    public function toArray()
-    {
-        return $this->elements;
-    }
-
-    /**
-     * @return History
-     */
-    public function first()
-    {
-        return reset($this->elements) ?: null;
-    }
-
-    /**
-     * @return History
-     */
-    public function last()
-    {
-        return end($this->elements) ?: null;
-    }
-
-    /**
-     * @param History $value
-     */
-    public function add(History $value)
-    {
-        $this->elements[] = $value;
-    }
 }
