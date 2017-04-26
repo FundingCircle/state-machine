4371c9bb5c548bd1746f11e084dee19929a69210
Nabeel Chaudhry <nabeel.chaudhry@fundingcircle.com>
2017-04-26 09:59:17 +0200

    excluded unnecessary setter

--- src/StateMachine/StateMachine/VersionInterface.php
+++ src/StateMachine/StateMachine/VersionInterface.php
@@ -10,9 +10,4 @@ interface VersionInterface
      * @return int
      */
     public function getVersion();
-
-    /**
-     * @param int $version
-     */
-    public function setVersion($version);
 }
